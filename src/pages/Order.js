def2d95e6ace72744c417a9cafa281d76bb0a907
Shiva Verma <shivaverma30july@gmail.com>
2024-04-26 12:59:44 +0530

    backend api changed

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -21,7 +21,7 @@ const Order = () => {
         }
     }, [setLogin, navigate])
     useEffect(()=>{
-        fetch(`${BackendURL}/getorder`,{
+        fetch(`${BackendURL}getorder`,{
             headers:{
                 "Content-Type":"application/json",
                 "Authorization":localStorage.getItem("token")
